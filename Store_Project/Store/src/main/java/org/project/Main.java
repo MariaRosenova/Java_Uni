a93d9db800e2b909c99db9e4f389c21ebeec9af9
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-05-31 10:43:53 +0300

    Test the calculation of the products

--- Store_Project/Store/src/main/java/org/project/Main.java
+++ Store_Project/Store/src/main/java/org/project/Main.java
@@ -1,7 +1,24 @@
 package org.project;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Create instances of SellingPriceCalculator
+        SellingPriceCalculator foodCalculator = new FoodSellingPriceCalculator();
+        SellingPriceCalculator nonFoodCalculator = new NonFoodSellingPriceCalculator();
+
+        // Injecting the dependencies into Product instances
+        Product foodProduct = new Product(1, "Apple", 100, "Food", LocalDate.now().plusDays(5), 10, foodCalculator);
+        Product nonFoodProduct = new Product(2, "Book", 50, "NonFood", null, 20, nonFoodCalculator);
+
+        // Calculate selling prices
+        double foodSellingPrice = foodProduct.calculateSellingPrice(20, 3, 10);
+        double nonFoodSellingPrice = nonFoodProduct.calculateSellingPrice(20, 0, 0);
+
+        // Output results
+        System.out.println("Food selling price: " + foodSellingPrice);
+        System.out.println("Non-food selling price: " + nonFoodSellingPrice);
     }
 }
