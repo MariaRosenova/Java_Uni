9d99fa2c0fff18734059e83c74ed2dc7169713ff
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-05-31 10:41:31 +0300

    Add dependency injection

--- Store_Project/Store/src/main/java/org/project/Product.java
+++ Store_Project/Store/src/main/java/org/project/Product.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 public class Product implements Serializable {
-    private static final long serialVersionUID = 1L;
 
     private int id;
     private String name;
@@ -12,28 +11,23 @@ public class Product implements Serializable {
     private String category;
     private LocalDate expirationDate;
     private int quantity;
+    private SellingPriceCalculator sellingPriceCalculator;
 
     // Constructor
-    public Product(int id, String name, double unitDeliveryPrice, String category, LocalDate expirationDate, int quantity) {
+    public Product(int id, String name, double unitDeliveryPrice, String category, LocalDate expirationDate, int quantity, SellingPriceCalculator sellingPriceCalculator) {
         this.id = id;
         this.name = name;
         this.unitDeliveryPrice = unitDeliveryPrice;
         this.category = category;
         this.expirationDate = expirationDate;
         this.quantity = quantity;
+        this.sellingPriceCalculator = sellingPriceCalculator;
     }
 
     // Method to calculate selling price
+    //ADD checking for food or non-food
     public double calculateSellingPrice(int markupPercentage, int daysBeforeExpiration, int discountPercentage) {
-        double sellingPrice = unitDeliveryPrice + (unitDeliveryPrice * markupPercentage / 100);
-
-        LocalDate expirationPlusDays = LocalDate.now().plusDays(daysBeforeExpiration);
-        if (expirationPlusDays.isAfter(expirationDate)) {
-            double discountAmount = sellingPrice * discountPercentage / 100;
-            sellingPrice -= discountAmount;
-        }
-
-        return sellingPrice;
+        return sellingPriceCalculator.calculateSellingPrice(unitDeliveryPrice, markupPercentage, discountPercentage, daysBeforeExpiration, expirationDate);
     }
 
     public int getId() {
